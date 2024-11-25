4a9949280c6a5e0e637fb5c034ea9ae65e148015
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2024-11-25 16:44:07 +0100

    init 2

---
+++ gls_strip_packing/src/io/mod.rs
@@ -0,0 +1,67 @@
+use std::fs;
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::path::Path;
+
+use log::{info, Level, LevelFilter, log};
+use svg::Document;
+
+use jagua_rs::io::json_instance::JsonInstance;
+
+use crate::EPOCH;
+pub mod layout_to_svg;
+pub mod svg_export;
+pub mod svg_util;
+pub fn read_json_instance(path: &Path) -> JsonInstance {
+    let file = File::open(path)
+        .unwrap_or_else(|err| panic!("could not open instance file: {}, {}", path.display(), err));
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader)
+        .unwrap_or_else(|err| panic!("could not parse instance file: {}, {}", path.display(), err))
+}
+
+pub fn write_svg(document: &Document, path: &Path) {
+    svg::save(path, document).expect("failed to write svg file");
+    info!(
+        "Solution SVG written to file://{}",
+        fs::canonicalize(&path)
+            .expect("could not canonicalize path")
+            .to_str()
+            .unwrap()
+    );
+}
+
+pub fn init_logger(level_filter: LevelFilter) {
+    fern::Dispatch::new()
+        // Perform allocation-free log formatting
+        .format(|out, message, record| {
+            let handle = std::thread::current();
+            let thread_name = handle.name().unwrap_or("-");
+
+            let duration = EPOCH.elapsed();
+            let sec = duration.as_secs() % 60;
+            let min = (duration.as_secs() / 60) % 60;
+            let hours = (duration.as_secs() / 60) / 60;
+
+            let prefix = format!(
+                "[{}] [{:0>2}:{:0>2}:{:0>2}] <{}>",
+                record.level(),
+                hours,
+                min,
+                sec,
+                thread_name,
+            );
+
+            out.finish(format_args!("{:<27}{}", prefix, message))
+        })
+        // Add blanket level filter -
+        .level(level_filter)
+        .chain(std::io::stdout())
+        .apply()
+        .expect("could not initialize logger");
+    log!(
+        Level::Info,
+        "Epoch: {}",
+        humantime::format_rfc3339_seconds(std::time::SystemTime::now())
+    );
+}
--- gls_strip_packing/src/main.rs
+++ gls_strip_packing/src/main.rs
@@ -1,3 +1,14 @@
+use std::time::Instant;
+use mimalloc::MiMalloc;
+use once_cell::sync::Lazy;
+
+mod io;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+pub static EPOCH: Lazy<Instant> = Lazy::new(Instant::now);
+
 fn main() {
     println!("Hello, world!");
 }
